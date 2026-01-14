9ce43ae8d285cebc8123379cb00244d87afc40cd
divya naik <dnaik6@charlotte.edu>
2026-01-13 23:33:18 -0600

    modified:   bank-zip/frontend/src/App.jsx

--- bank-zip/frontend/src/App.jsx
+++ bank-zip/frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Homepage from "../components/Home";
+import Dashboard from "../components/Admin";
 const App = () =>{
   return (
     <BrowserRouter>
