a96eeaa399a756d67591d899d4dd1169f453177a
divya naik <dnaik6@charlotte.edu>
2026-01-13 22:56:27 -0600

    add the beginnings of frontend and add backend template

---
+++ bank-zip/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Homepage from "../components/Home";
+const App = () =>{
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </BrowserRouter> 
+  )
+}
+export default App;
