f6537374acad544afb13c3f6a5a87c0b0c9d78e1
divya naik <dnaik6@charlotte.edu>
2026-01-14 19:57:25 -0600

    modified:   bank-zip/backend/app.js 	deleted:    bank-zip/backend/routes/index.js 	deleted:    bank-zip/backend/routes/users.js 	new file:   bank-zip/backend/routes/users.routes.js 	new file:   bank-zip/backend/services/db.service.js

--- bank-zip/backend/app.js
+++ bank-zip/backend/app.js
@@ -7,6 +7,9 @@ const logger = require('morgan');
 
 const app = express();
 
+// require routers files
+const usersRouter = require('./routes/users.routes');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -17,7 +20,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+// route level middleware
+app.use('/api/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
