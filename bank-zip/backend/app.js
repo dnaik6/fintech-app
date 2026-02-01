a676c691db38577dfad1b9f1c5c2fd579733aac9
divya naik <dnaik6@charlotte.edu>
2026-02-01 03:25:42 -0600

    modified:   bank-zip/backend/app.js

--- bank-zip/backend/app.js
+++ bank-zip/backend/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 // require routers files
 const usersRouter = require('./routes/users.routes');
-
+const uploadRouter = require('./routes/upload.routes');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // route level middleware
 app.use('/api/users', usersRouter);
-
+app.use('/api/upload', uploadRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
